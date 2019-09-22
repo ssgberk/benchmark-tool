43514f302c7a4e81f4236d0ed5dfa8f2a84c8d29
Matheus Rocha Vieira <matheusrv@email.com>
2019-09-22 03:57:49 -0300

    Remove NodeModules && Convert all files to LF

--- toolset/wrk/concurrency.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#echo " %wrk -H 'Host: $server_host' -H 'Accept: $accept' -H 'Connection: keep-alive' --latency -d 5 -c 8 --timeout 8 -t 8 $url"
-hyperfine --export-json hugo_throughputTest_date.json 'hugo' --show-output
-sleep 5
